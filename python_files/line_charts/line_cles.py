d589972f4e166b5f832c6977434d387fef3812de
lllinares <laurentllinares@laposte.net>
2021-04-08 20:52:05 +0200

    departments cases also available

--- python_files/line_charts/line_cles.py
+++ python_files/line_charts/line_cles.py
@@ -107,10 +107,6 @@ def keyseries(nom,chiffre,evo=True):
             return df_covid[chiffre].diff()
         return df_covid[chiffre]
     elif chiffre in ["cas_confirmes"]:
-        urlposreg="https://www.data.gouv.fr/fr/datasets/r/001aca18-df6a-45c8-89e6-f82d689e6c01"
-        pathtarget="../data/posquotreg.csv"
-        download(urlposreg,pathtarget,replace=True)
-        dfposreg=pd.read_csv(pathtarget,sep=";")
         REGIONS = {
     'Auvergne-Rhône-Alpes': 84,
     'Bourgogne-Franche-Comté': 27,
@@ -129,14 +125,136 @@ def keyseries(nom,chiffre,evo=True):
     'Occitanie': 76,
     'Pays de la Loire': 52,
     'Provence-Alpes-Côte d\'Azur': 93,}
-        number=REGIONS[nom]
 
-        df= dfposreg.loc[dfposreg["reg"]==number,:].groupby(['jour']).sum()
-        df.index = pd.to_datetime(df.index)
+        DEPARTMENTS = {
+    '01': 'Ain', 
+    '02': 'Aisne', 
+    '03': 'Allier', 
+    '04': 'Alpes-de-Haute-Provence', 
+    '05': 'Hautes-Alpes',
+    '06': 'Alpes-Maritimes', 
+    '07': 'Ardèche', 
+    '08': 'Ardennes', 
+    '09': 'Ariège', 
+    '10': 'Aube', 
+    '11': 'Aude',
+    '12': 'Aveyron', 
+    '13': 'Bouches-du-Rhône', 
+    '14': 'Calvados', 
+    '15': 'Cantal', 
+    '16': 'Charente',
+    '17': 'Charente-Maritime', 
+    '18': 'Cher', 
+    '19': 'Corrèze', 
+    '2A': 'Corse-du-Sud', 
+    '2B': 'Haute-Corse',
+    '21': 'Côte-d\'Or', 
+    '22': 'Côtes-d\'Armor', 
+    '23': 'Creuse', 
+    '24': 'Dordogne', 
+    '25': 'Doubs', 
+    '26': 'Drôme',
+    '27': 'Eure', 
+    '28': 'Eure-et-Loir', 
+    '29': 'Finistère', 
+    '30': 'Gard', 
+    '31': 'Haute-Garonne', 
+    '32': 'Gers',
+    '33': 'Gironde', 
+    '34': 'Hérault', 
+    '35': 'Ille-et-Vilaine', 
+    '36': 'Indre', 
+    '37': 'Indre-et-Loire',
+    '38': 'Isère', 
+    '39': 'Jura', 
+    '40': 'Landes', 
+    '41': 'Loir-et-Cher', 
+    '42': 'Loire', 
+    '43': 'Haute-Loire',
+    '44': 'Loire-Atlantique', 
+    '45': 'Loiret', 
+    '46': 'Lot', 
+    '47': 'Lot-et-Garonne', 
+    '48': 'Lozère',
+    '49': 'Maine-et-Loire', 
+    '50': 'Manche', 
+    '51': 'Marne', 
+    '52': 'Haute-Marne', 
+    '53': 'Mayenne',
+    '54': 'Meurthe-et-Moselle', 
+    '55': 'Meuse', 
+    '56': 'Morbihan', 
+    '57': 'Moselle', 
+    '58': 'Nièvre', 
+    '59': 'Nord',
+    '60': 'Oise', 
+    '61': 'Orne', 
+    '62': 'Pas-de-Calais', 
+    '63': 'Puy-de-Dôme', 
+    '64': 'Pyrénées-Atlantiques',
+    '65': 'Hautes-Pyrénées', 
+    '66': 'Pyrénées-Orientales', 
+    '67': 'Bas-Rhin', 
+    '68': 'Haut-Rhin', 
+    '69': 'Rhône',
+    '70': 'Haute-Saône', 
+    '71': 'Saône-et-Loire', 
+    '72': 'Sarthe', 
+    '73': 'Savoie', 
+    '74': 'Haute-Savoie',
+    '75': 'Paris', 
+    '76': 'Seine-Maritime', 
+    '77': 'Seine-et-Marne', 
+    '78': 'Yvelines', 
+    '79': 'Deux-Sèvres',
+    '80': 'Somme', 
+    '81': 'Tarn', 
+    '82': 'Tarn-et-Garonne', 
+    '83': 'Var', 
+    '84': 'Vaucluse', 
+    '85': 'Vendée',
+    '86': 'Vienne', 
+    '87': 'Haute-Vienne', 
+    '88': 'Vosges', 
+    '89': 'Yonne', 
+    '90': 'Territoire de Belfort',
+    '91': 'Essonne', 
+    '92': 'Hauts-de-Seine', 
+    '93': 'Seine-Saint-Denis', 
+    '94': 'Val-de-Marne', 
+    '95': 'Val-d\'Oise',
+    '971': 'Guadeloupe', 
+    '972': 'Martinique', 
+    '973': 'Guyane', 
+    '974': 'La Réunion', 
+    '976': 'Mayotte',
+}
+        if nom in REGIONS.keys():
+            urlposreg="https://www.data.gouv.fr/fr/datasets/r/001aca18-df6a-45c8-89e6-f82d689e6c01"
+            pathtarget="../data/posquotreg.csv"
+            download(urlposreg,pathtarget,replace=True)
+            dfposreg=pd.read_csv(pathtarget,sep=";")
+            number=REGIONS[nom]
 
-        if evo:
-            return df['P']
-        else: return df['P'].cumsum()
+            df= dfposreg.loc[dfposreg["reg"]==number,:].groupby(['jour']).sum()
+            df.index = pd.to_datetime(df.index)
+
+            if evo:
+                return df['P']
+            else: return df['P'].cumsum()
+        if nom in DEPARTMENTS.keys():
+            urlposdep="https://www.data.gouv.fr/fr/datasets/r/406c6a23-e283-4300-9484-54e78c8ae675"
+            pathtarget="../data/posquotdep.csv"
+            download(urlposdep,pathtarget,replace=True)
+            dfposdep=pd.read_csv(pathtarget,sep=";")
+            number=DEPARTMENTS[nom]
+
+            df= dfposDEP.loc[dfposDEP["reg"]==number,:].groupby(['jour']).sum()
+            df.index = pd.to_datetime(df.index)
+            if evo:
+                return df['P']
+            else: return df['P'].cumsum()
+
 
     if evo:
         return keysubtablename(nom)[chiffre].dropna().diff()
