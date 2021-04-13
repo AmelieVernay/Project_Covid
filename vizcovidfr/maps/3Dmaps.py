1dd147ac811480e8bf843f00d88303db0e6b2dfb
AmelieVernay <amelie.vernay@etu.umontpellier.fr>
2021-04-13 18:36:35 +0200

    add 3Dmap function

--- vizcovidfr/maps/3Dmaps.py
+++ vizcovidfr/maps/3Dmaps.py
@@ -1,12 +1,90 @@
 # ---------- requirements ----------
 
-# local reqs
+import os
 
 # map reqs
-import pydeck as pdk
-
 import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+import pydeck as pdk
 from vega_datasets import data as vds
 import ipywidgets
 from palettable.cartocolors.sequential import BrwnYl_3
 import json
+
+# local reqs
+from vizcovidfr.loads import load_datasets
+from vizcovidfr.preprocesses import preprocess_chiffres_cles
+
+
+def make_3D_map(granularity):
+    # ---------- file imports ----------
+    # geo files
+    reg_path = os.path.join(
+                    os.path.dirname(
+                        os.path.realpath(__file__)),
+                    "geodata", "regions.geojson")
+    dep_path = os.path.join(
+                    os.path.dirname(
+                        os.path.realpath(__file__)),
+                    "geodata", "departements.geojson")
+    reg = gpd.read_file(reg_path)
+    dep = gpd.read_file(dep_path)
+    # covid files
+    df_covid = load_datasets.Load_chiffres_cles().save_as_df()
+    # ---------- preprocesses ----------
+    df_covid = preprocess_chiffres_cles.drop_some_columns(df_covid)
+    df_covid = preprocess_chiffres_cles.reg_depts(df_covid)
+    df_covid = preprocess_chiffres_cles.reg_depts_code_format(df_covid)
+    # choose dataframe according to granularity argument
+    if (granularity == 'departement'):
+        df = dep.copy()
+        gra = 'department'
+    else:
+        df = reg.copy()
+        gra = 'region'
+    # for covid data
+    df_local = df_covid.loc[df_covid['granularite'] == granularity]
+    # for geo data
+    # grab department's centroids (lat and lon)
+    df_points = df.copy()
+    df_points['geometry'] = df_points['geometry'].centroid
+    # merging on 'code'
+    A = pd.merge(df_local, df_points, on='code')
+    # separate latitude and longitude
+    A['lon'] = A.geometry.apply(lambda p: p.x)
+    A['lat'] = A.geometry.apply(lambda p: p.y)
+    # ---------- make map! ----------
+    # initialize view (centered on Paris!)
+    view = pdk.ViewState(latitude=46.232192999999995,
+                         longitude=2.209666999999996,
+                         pitch=50,
+                         zoom=6)
+    # add pydeck layers
+    covid_amount_layer = pdk.Layer('ColumnLayer',
+                                   data=A,
+                                   get_position=['lon', 'lat'],
+                                   get_elevation='deces',
+                                   elevation_scale=100,
+                                   radius=7000,
+                                   get_fill_color=[255, 165, 0, 80],
+                                   pickable=True,
+                                   auto_highlight=True)
+    # render map
+    covid_amount_layer_map = pdk.Deck(layers=covid_amount_layer,
+                                      initial_view_state=view)
+    # save map
+    covid_amount_layer_map.to_html('3Dmap_covid.html')
+
+
+# To try:
+# make_3D_map('region')
+
+# TODO:
+# ADD DOCSTRING !!
+# add possibility to change 'deces'
+# change save path
+# try to make polygon layers instead of ColumnLayer
+# change markers aspect
+# change color ?
--- vizcovidfr/maps/maps.py
+++ vizcovidfr/maps/maps.py
@@ -6,7 +6,7 @@ import pandas as pd
 from vizcovidfr.loads import load_datasets
 from vizcovidfr.preprocesses import preprocess_chiffres_cles
 
-df_covid = load_datasets.Load_chiffres_cles().save_as_df()
+#df_covid = load_datasets.Load_chiffres_cles().save_as_df()
 
 # functions in preprocess_chiffres_cles should be used as follow:
 # A = preprocess_chiffres_cles.drop_some_columns(df_covid)
@@ -16,74 +16,74 @@ df_covid = load_datasets.Load_chiffres_cles().save_as_df()
 
 
 # ---------- main function ----------
-def vizmap_death(granularity):
-    '''
-    Input:
-        granularity: string corresponding to the scale
-                     we want the map to have
-    Output:
-        a map representing the actual number of death per
-        region or department (according to the granularity chosen)
-    '''
-    # load the dataframes
-    dpt = gpd.read_file('departements-version-simplifiee.geojson')
-    rgn = gpd.read_file('regions-version-simplifiee.geojson')
-    df_covid = load_datasets.Load_chiffres_cles().save_as_df()
-    # choose the dataframe containing geographic
-    # informations according to the granularity
-    # (plus english precision)
-    if (granularity == 'departement'):
-        df = dpt.copy()
-        gra = 'department'
-    else:
-        df = rgn.copy()
-        gra = 'region'
-    # only data concerning the chosen granularity
-    df_local = df_covid.loc[df_covid['granularite'] == granularity]
-    df_local = df_local.dropna(subset=['deces'])
-    # keep only relevent informations
-    df1 = df_local[['maille_code', 'maille_nom', 'deces']]
-    df1 = df1.groupby(['maille_nom']).max()
-    # format the 'code' strings
-    for i in range(len(df1['maille_code'])):
-        df1['maille_code'].iloc[i] = df1['maille_code'].iloc[i][4:]
-    df1['code'] = df1['maille_code']
-    del df1['maille_code']
-    # set common index
-    df1.set_index('code', inplace=True)
-    df.set_index('code', inplace=True)
-    # define new joined GeoDataFrame
-    together = df1.join(df)
-    together = gpd.GeoDataFrame(together)
-    # ----- Plotting part -----
-    # set a variable that will call whatever column
-    # we want to visualise on the map
-    variable = 'deces'
-    # set range for the choropleth
-    vmin = together['deces'].min()
-    vmax = together['deces'].max()
-    # create figure and axes
-    fig, ax = plt.subplots(1, figsize=(16, 7))
-    # create map
-    together.plot(column=variable,
-                  cmap=('Reds'),
-                  linewidth=0.8,
-                  ax=ax,
-                  edgecolor='0.6')
-    # remove the axis
-    ax.axis('off')
-    # add a title
-    # make it correspond to the granularity!
-    ax.set_title('Actual number of death per {}'.format(gra),
-                 fontdict={'fontsize': '25',
-                           'fontweight': '4'})
-    # create colorbar as a legend
-    sm = plt.cm.ScalarMappable(
-        cmap='Reds',
-        norm=plt.Normalize(vmin=vmin, vmax=vmax))
-    sm._A = []
-    cbar = fig.colorbar(sm)
-    fig.savefig('toto.svg')
-
-
+#def vizmap_death(granularity):
+#    '''
+#    Input:
+#        granularity: string corresponding to the scale
+#                     we want the map to have
+#    Output:
+#        a map representing the actual number of death per
+#        region or department (according to the granularity chosen)
+#    '''
+#    # load the dataframes
+#    dpt = gpd.read_file('departements-version-simplifiee.geojson')
+#    rgn = gpd.read_file('regions-version-simplifiee.geojson')
+#    df_covid = load_datasets.Load_chiffres_cles().save_as_df()
+#    # choose the dataframe containing geographic
+#    # informations according to the granularity
+#    # (plus english precision)
+#    if (granularity == 'departement'):
+#        df = dpt.copy()
+#        gra = 'department'
+#    else:
+#        df = rgn.copy()
+#        gra = 'region'
+#    # only data concerning the chosen granularity
+#    df_local = df_covid.loc[df_covid['granularite'] == granularity]
+#    df_local = df_local.dropna(subset=['deces'])
+#    # keep only relevent informations
+#    df1 = df_local[['maille_code', 'maille_nom', 'deces']]
+#    df1 = df1.groupby(['maille_nom']).max()
+#    # format the 'code' strings
+#    for i in range(len(df1['maille_code'])):
+#        df1['maille_code'].iloc[i] = df1['maille_code'].iloc[i][4:]
+#    df1['code'] = df1['maille_code']
+#    del df1['maille_code']
+#    # set common index
+#    df1.set_index('code', inplace=True)
+#    df.set_index('code', inplace=True)
+#    # define new joined GeoDataFrame
+#    together = df1.join(df)
+#    together = gpd.GeoDataFrame(together)
+#    # ----- Plotting part -----
+#    # set a variable that will call whatever column
+#    # we want to visualise on the map
+#    variable = 'deces'
+#    # set range for the choropleth
+#    vmin = together['deces'].min()
+#    vmax = together['deces'].max()
+#    # create figure and axes
+#    fig, ax = plt.subplots(1, figsize=(16, 7))
+#    # create map
+#    together.plot(column=variable,
+#                  cmap=('Reds'),
+#                  linewidth=0.8,
+#                  ax=ax,
+#                  edgecolor='0.6')
+#    # remove the axis
+#    ax.axis('off')
+#    # add a title
+#    # make it correspond to the granularity!
+#    ax.set_title('Actual number of death per {}'.format(gra),
+#                 fontdict={'fontsize': '25',
+#                           'fontweight': '4'})
+#    # create colorbar as a legend
+#    sm = plt.cm.ScalarMappable(
+#        cmap='Reds',
+#        norm=plt.Normalize(vmin=vmin, vmax=vmax))
+#    sm._A = []
+#    cbar = fig.colorbar(sm)
+#    fig.savefig('toto.svg')
+#
+#
 # vizmap('departement')
