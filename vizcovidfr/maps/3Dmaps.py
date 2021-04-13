cee43271701c278a68f9582b346c1452a1751454
AmelieVernay <amelie.vernay@etu.umontpellier.fr>
2021-04-13 16:17:05 +0200

    add 3D maps

---
+++ vizcovidfr/maps/3Dmaps.py
@@ -0,0 +1,12 @@
+# ---------- requirements ----------
+
+# local reqs
+
+# map reqs
+import pydeck as pdk
+
+import pandas as pd
+from vega_datasets import data as vds
+import ipywidgets
+from palettable.cartocolors.sequential import BrwnYl_3
+import json
