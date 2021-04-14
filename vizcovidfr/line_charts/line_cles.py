7f8fd48ba195566c63e79b09edadcedf196938f8
AmelieVernay <amelie.vernay@etu.umontpellier.fr>
2021-04-14 11:15:15 +0200

    change bad file name

--- vizcovidfr/line_charts/line_cles.py
+++ vizcovidfr/line_charts/line_cles.py
@@ -11,8 +11,8 @@ url_fr="https://www.data.gouv.fr/fr/datasets/r/d3a98a30-893f-47f7-96c5-2f4bcaaa0
 path_target = "../data/chiffres-cles.csv"
 class Load_covid:
     """
-    A class that allows you to download and format the dataset 
-    containing the counts of diverse figures describing the 
+    A class that allows you to download and format the dataset
+    containing the counts of diverse figures describing the
     spread of the pandemic in France.
 
     """
@@ -35,13 +35,13 @@ class Load_covid:
         df_covid = df_covid.set_index('date')
         df_covid
         return df_covid['2020-01-24':]
-    
-    
+
+
 def keysubtablename(nom):
     """
     nom: A part of France or a partition
 
-    Function that extract the data for a certain 
+    Function that extract the data for a certain
     granularity or territory and remove repetitions.
     """
     df_covid=Load_covid().save_as_df()
@@ -65,7 +65,7 @@ def keyseries(nom,chiffre,evo=True):
     fr=nom=="France"
     if chiffre in ["deces_à_l'hôpital","deceshop"]:
         chiffre="total_deces_hopital"
-        fr=True 
+        fr=True
         print("données pour la France entière")
 
     if fr:
@@ -127,106 +127,106 @@ def keyseries(nom,chiffre,evo=True):
     'Provence-Alpes-Côte d\'Azur': 93,}
 
         DEPARTMENTS = {
-    '01': 'Ain', 
-    '02': 'Aisne', 
-    '03': 'Allier', 
-    '04': 'Alpes-de-Haute-Provence', 
+    '01': 'Ain',
+    '02': 'Aisne',
+    '03': 'Allier',
+    '04': 'Alpes-de-Haute-Provence',
     '05': 'Hautes-Alpes',
-    '06': 'Alpes-Maritimes', 
-    '07': 'Ardèche', 
-    '08': 'Ardennes', 
-    '09': 'Ariège', 
-    '10': 'Aube', 
+    '06': 'Alpes-Maritimes',
+    '07': 'Ardèche',
+    '08': 'Ardennes',
+    '09': 'Ariège',
+    '10': 'Aube',
     '11': 'Aude',
-    '12': 'Aveyron', 
-    '13': 'Bouches-du-Rhône', 
-    '14': 'Calvados', 
-    '15': 'Cantal', 
+    '12': 'Aveyron',
+    '13': 'Bouches-du-Rhône',
+    '14': 'Calvados',
+    '15': 'Cantal',
     '16': 'Charente',
-    '17': 'Charente-Maritime', 
-    '18': 'Cher', 
-    '19': 'Corrèze', 
-    '2A': 'Corse-du-Sud', 
+    '17': 'Charente-Maritime',
+    '18': 'Cher',
+    '19': 'Corrèze',
+    '2A': 'Corse-du-Sud',
     '2B': 'Haute-Corse',
-    '21': 'Côte-d\'Or', 
-    '22': 'Côtes-d\'Armor', 
-    '23': 'Creuse', 
-    '24': 'Dordogne', 
-    '25': 'Doubs', 
+    '21': 'Côte-d\'Or',
+    '22': 'Côtes-d\'Armor',
+    '23': 'Creuse',
+    '24': 'Dordogne',
+    '25': 'Doubs',
     '26': 'Drôme',
-    '27': 'Eure', 
-    '28': 'Eure-et-Loir', 
-    '29': 'Finistère', 
-    '30': 'Gard', 
-    '31': 'Haute-Garonne', 
+    '27': 'Eure',
+    '28': 'Eure-et-Loir',
+    '29': 'Finistère',
+    '30': 'Gard',
+    '31': 'Haute-Garonne',
     '32': 'Gers',
-    '33': 'Gironde', 
-    '34': 'Hérault', 
-    '35': 'Ille-et-Vilaine', 
-    '36': 'Indre', 
+    '33': 'Gironde',
+    '34': 'Hérault',
+    '35': 'Ille-et-Vilaine',
+    '36': 'Indre',
     '37': 'Indre-et-Loire',
-    '38': 'Isère', 
-    '39': 'Jura', 
-    '40': 'Landes', 
-    '41': 'Loir-et-Cher', 
-    '42': 'Loire', 
+    '38': 'Isère',
+    '39': 'Jura',
+    '40': 'Landes',
+    '41': 'Loir-et-Cher',
+    '42': 'Loire',
     '43': 'Haute-Loire',
-    '44': 'Loire-Atlantique', 
-    '45': 'Loiret', 
-    '46': 'Lot', 
-    '47': 'Lot-et-Garonne', 
+    '44': 'Loire-Atlantique',
+    '45': 'Loiret',
+    '46': 'Lot',
+    '47': 'Lot-et-Garonne',
     '48': 'Lozère',
-    '49': 'Maine-et-Loire', 
-    '50': 'Manche', 
-    '51': 'Marne', 
-    '52': 'Haute-Marne', 
+    '49': 'Maine-et-Loire',
+    '50': 'Manche',
+    '51': 'Marne',
+    '52': 'Haute-Marne',
     '53': 'Mayenne',
-    '54': 'Meurthe-et-Moselle', 
-    '55': 'Meuse', 
-    '56': 'Morbihan', 
-    '57': 'Moselle', 
-    '58': 'Nièvre', 
+    '54': 'Meurthe-et-Moselle',
+    '55': 'Meuse',
+    '56': 'Morbihan',
+    '57': 'Moselle',
+    '58': 'Nièvre',
     '59': 'Nord',
-    '60': 'Oise', 
-    '61': 'Orne', 
-    '62': 'Pas-de-Calais', 
-    '63': 'Puy-de-Dôme', 
+    '60': 'Oise',
+    '61': 'Orne',
+    '62': 'Pas-de-Calais',
+    '63': 'Puy-de-Dôme',
     '64': 'Pyrénées-Atlantiques',
-    '65': 'Hautes-Pyrénées', 
-    '66': 'Pyrénées-Orientales', 
-    '67': 'Bas-Rhin', 
-    '68': 'Haut-Rhin', 
+    '65': 'Hautes-Pyrénées',
+    '66': 'Pyrénées-Orientales',
+    '67': 'Bas-Rhin',
+    '68': 'Haut-Rhin',
     '69': 'Rhône',
-    '70': 'Haute-Saône', 
-    '71': 'Saône-et-Loire', 
-    '72': 'Sarthe', 
-    '73': 'Savoie', 
+    '70': 'Haute-Saône',
+    '71': 'Saône-et-Loire',
+    '72': 'Sarthe',
+    '73': 'Savoie',
     '74': 'Haute-Savoie',
-    '75': 'Paris', 
-    '76': 'Seine-Maritime', 
-    '77': 'Seine-et-Marne', 
-    '78': 'Yvelines', 
+    '75': 'Paris',
+    '76': 'Seine-Maritime',
+    '77': 'Seine-et-Marne',
+    '78': 'Yvelines',
     '79': 'Deux-Sèvres',
-    '80': 'Somme', 
-    '81': 'Tarn', 
-    '82': 'Tarn-et-Garonne', 
-    '83': 'Var', 
-    '84': 'Vaucluse', 
+    '80': 'Somme',
+    '81': 'Tarn',
+    '82': 'Tarn-et-Garonne',
+    '83': 'Var',
+    '84': 'Vaucluse',
     '85': 'Vendée',
-    '86': 'Vienne', 
-    '87': 'Haute-Vienne', 
-    '88': 'Vosges', 
-    '89': 'Yonne', 
+    '86': 'Vienne',
+    '87': 'Haute-Vienne',
+    '88': 'Vosges',
+    '89': 'Yonne',
     '90': 'Territoire de Belfort',
-    '91': 'Essonne', 
-    '92': 'Hauts-de-Seine', 
-    '93': 'Seine-Saint-Denis', 
-    '94': 'Val-de-Marne', 
+    '91': 'Essonne',
+    '92': 'Hauts-de-Seine',
+    '93': 'Seine-Saint-Denis',
+    '94': 'Val-de-Marne',
     '95': 'Val-d\'Oise',
-    '971': 'Guadeloupe', 
-    '972': 'Martinique', 
-    '973': 'Guyane', 
-    '974': 'La Réunion', 
+    '971': 'Guadeloupe',
+    '972': 'Martinique',
+    '973': 'Guyane',
+    '974': 'La Réunion',
     '976': 'Mayotte',
 }
         if nom in REGIONS.keys():
@@ -278,7 +278,7 @@ def plotseries(series,average=True):
     else:
         ax=series.plot()
     return ax
-    
+
 def keyplot(nom,chiffre,evo=True,average=True):
 
     """
@@ -334,12 +334,10 @@ def keyplot(nom,chiffre,evo=True,average=True):
 
 
 keyplot("France","cas",evo=True)
-    
+
 print('a')
-keyseries("France","hôpital",evo=False)
-print("b")
-keyseries('Île-de-France','cas')
-print("c")
-keyplot('Île-de-France','cas')
-
-
+#keyseries("France","hôpital",evo=False)
+#print("b")
+#keyseries('Île-de-France','cas')
+#print("c")
+#keyplot('Île-de-France','cas')
