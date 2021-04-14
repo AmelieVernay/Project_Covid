88177968a9d8de12e3c340c413bbc7ac55d34364
lllinares <laurentllinares@laposte.net>
2021-04-14 22:06:21 +0200

    for departments also

--- vizcovidfr/line_charts/line_cles.py
+++ vizcovidfr/line_charts/line_cles.py
@@ -8,7 +8,9 @@ import seaborn as sns
 #url_db = "https://www.data.gouv.fr/en/datasets/r/0b66ca39-1623-4d9c-83ad-5434b7f9e2a4"
 url_db="https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv"
 url_fr="https://www.data.gouv.fr/fr/datasets/r/d3a98a30-893f-47f7-96c5-2f4bcaaa0d71"
+
 path_target = "../data/chiffres-cles.csv"
+
 class Load_covid:
     """
     A class that allows you to download and format the dataset
@@ -17,42 +19,56 @@ class Load_covid:
 
     """
     def __init__(self, url=url_db, target_name=path_target):
+
         download(url, target_name, replace=True)
 
     @staticmethod
     def save_as_df():
+
         """
         Nicely formatted dataframe with time indexation.
         """
+
         df_covid = pd.read_csv(path_target)
+
         date = df_covid['date']
         a = list((date[:]))
         b = list(map(lambda x: datetime.date(int(x[:4]),\
         int(x[5:7]), int(x[8:])).\
         isoformat(),a))
-        b = pd.DatetimeIndex(b)
+        b = pd.DatetimeIndex(b) #put a correct way to view date
         df_covid.loc[:, 'date'] = b
         df_covid = df_covid.set_index('date')
         df_covid
-        return df_covid['2020-01-24':]
+
+        return df_covid['2020-01-24':] #not have wrong data
 
 
 def keysubtablename(nom):
+
     """
     nom: A part of France or a partition
 
     Function that extract the data for a certain
     granularity or territory and remove repetitions.
     """
+
     df_covid=Load_covid().save_as_df()
+
     if nom in ["departements","pays","region"]:
+
         dfsub = df_covid.loc[df_covid['granularite']==nom]
+
     else:
         dfsub = df_covid.loc[df_covid['maille_nom']==nom]
-    dfsub = dfsub[~dfsub.index.duplicated(keep='first')]
+
+    dfsub = dfsub[~dfsub.index.duplicated(keep='first')] #have an 
+    #inforamtion once
+
     return dfsub
 
 def keyseries(nom,chiffre,evo=True):
+
     """
 
     nom: A part of France
@@ -62,15 +78,19 @@ def keyseries(nom,chiffre,evo=True):
     Give the time series of the figure of interest
 
     """
-    fr=nom=="France"
+
+    fr= nom=="France"
+
     if chiffre in ["deces_à_l'hôpital","deceshop"]:
+
         chiffre="total_deces_hopital"
-        fr=True
-        print("données pour la France entière")
+        fr=True #no big difference with only "deces"
 
     if fr:
+
         download(url_fr,"../data/chiffres-fr.csv",replace=True)
         df_covid=pd.read_csv("../data/chiffres-fr.csv")
+
         date = df_covid['date']
         a = list((date[:]))
         b = list(map(lambda x: datetime.date(int(x[:4]),int(x[5:7]),\
@@ -79,34 +99,62 @@ def keyseries(nom,chiffre,evo=True):
         b = pd.DatetimeIndex(b)
         df_covid.loc[:, 'date'] = b
         df_covid = df_covid.set_index('date')
+
     if chiffre in ["cas","nombre_de_cas","cas_confirmes"]:
+
         chiffre="cas_confirmes"
+
         if fr:
+
             chiffre="total_cas_confirmes"
+
     elif chiffre in ["hospitalisation","hôpital","hospitalises"]:
+        
         chiffre="hospitalises"
+
         if fr:
+
             chiffre="patients_hospitalises"
+
     elif chiffre in ["deces_ehpad"]:
+
         if fr:
+
             chiffre="total_deces_ehpad"
+
     elif chiffre in ["morts"]:
+
         chiffre="deces"
+
     elif chiffre in ["reanimation"]:
+
         if fr:
+
             chiffre="patients_reanimation"
 
     elif chiffre in ["cas_confirmes_ehpad"]:
+
         if fr:
+
             chiffre="total_cas_confirmes_ephad"
+
     elif chiffre in ["gueris"]:
+
         if fr:
-            chiffre="total_patients_gueris"
+
+            chiffre="total_patients_gueris" #options with
+            # different expressions for a same argument
+
     if fr:
+
         if evo:
+
             return df_covid[chiffre].diff()
+
         return df_covid[chiffre]
-    elif chiffre in ["cas_confirmes"]:
+
+    elif chiffre in ["cas_confirmes"]: #need specific datasets
+
         REGIONS = {
     'Auvergne-Rhône-Alpes': 84,
     'Bourgogne-Franche-Comté': 27,
@@ -126,7 +174,7 @@ def keyseries(nom,chiffre,evo=True):
     'Pays de la Loire': 52,
     'Provence-Alpes-Côte d\'Azur': 93,}
 
-        DEPARTMENTS = {
+        DEPARTMENTS = { #match between number and territory
     '01': 'Ain',
     '02': 'Aisne',
     '03': 'Allier',
@@ -230,34 +278,52 @@ def keyseries(nom,chiffre,evo=True):
     '976': 'Mayotte',
 }
         if nom in REGIONS.keys():
+
             urlposreg="https://www.data.gouv.fr/fr/datasets/r/001aca18-df6a-45c8-89e6-f82d689e6c01"
             pathtarget="../data/posquotreg.csv"
             download(urlposreg,pathtarget,replace=True)
             dfposreg=pd.read_csv(pathtarget,sep=";")
             number=REGIONS[nom]
 
-            df= dfposreg.loc[dfposreg["reg"]==number,:].groupby(['jour']).sum()
+            df= dfposreg.loc[dfposreg["reg"]==number,:]
+            df=df.loc[df["cl_age90"]==0]
             df.index = pd.to_datetime(df.index)
 
             if evo:
+
                 return df['P']
-            else: return df['P'].cumsum()
+
+            else: 
+                
+                return df['P'].cumsum()
+
         if nom in DEPARTMENTS.keys():
+
             urlposdep="https://www.data.gouv.fr/fr/datasets/r/406c6a23-e283-4300-9484-54e78c8ae675"
             pathtarget="../data/posquotdep.csv"
             download(urlposdep,pathtarget,replace=True)
             dfposdep=pd.read_csv(pathtarget,sep=";")
             number=DEPARTMENTS[nom]
 
-            df= dfposDEP.loc[dfposDEP["reg"]==number,:].groupby(['jour']).sum()
+            df= dfposdep.loc[dfposdep["dep"]==number,:]
+            df=df.loc[df["cl_age90"]==0]
+
+
             df.index = pd.to_datetime(df.index)
+
             if evo:
+
                 return df['P']
-            else: return df['P'].cumsum()
+
+            else: 
+                
+                return df['P'].cumsum()
 
 
     if evo:
+
         return keysubtablename(nom)[chiffre].dropna().diff()
+
     return keysubtablename(nom)[chiffre].dropna()
 
 
@@ -273,10 +339,15 @@ def plotseries(series,average=True):
 
     """
     sns.set(rc={'figure.figsize':(11, 4)})
+
     if average:
+
         ax=series.rolling(window=7).mean().plot()
+
     else:
+
         ax=series.plot()
+
     return ax
 
 def keyplot(nom,chiffre,evo=True,average=True):
@@ -290,45 +361,75 @@ def keyplot(nom,chiffre,evo=True,average=True):
     Plot the time series associates with figures of Covid-19.
     Take in acount the scale (country, region, ...)
     """
+
     ax=plotseries(keyseries(nom,chiffre,evo),average)
+
     if chiffre in ["cas","nombre_de_cas","cas_confirmes"] and not evo :
+
         ax.set(title= "Prevalence of Covid-19 in "+nom,ylabel="case")
+
     elif chiffre in ["cas","nombre_de_cas","cas_confirmes"] and evo:
+
         ax.set(title= "Daily cases of Covid-19 in "+nom,ylabel="case")
+
     elif chiffre in ["hospitalisation","hôpital","hospitalises"] and evo:
+
         ax.set(title= "Daily extra patients of Covid-19\
              at the hospital in "+nom,ylabel="people hospitalized")
+
     elif chiffre in ["hospitalisation","hôpital","hospitalises"] and not evo:
+
         ax.set(title= "Number of patients of Covid-19\
          at the hospital in "+nom,ylabel="people hospitalized")
+
     elif chiffre in["deces_ehpad"] and not evo:
+
         ax.set(title= "Number of death of Covid-19 \
             in EHPADs in "+nom,ylabel="death")
+
     elif chiffre in["deces_ehpad"] and  evo:
+
         ax.set(title= "Number of death of Covid-19 in\
              EHPADs in "+nom,ylabel="death")
+
     elif chiffre in["deces","morts"] and  not evo:
+
         ax.set(title= "Number of deaths of Covid-19 \
              in "+nom,ylabel="death")
+
     elif chiffre in["deces","morts"] and   evo:
+
         ax.set(title= "New deaths of Covid-19\
               in "+nom,ylabel="death")
+
     elif chiffre in["reanimation"] and   evo:
+
         ax.set(title= "Daily extra patients in\
-             intensive care because of Covid-19  in "+nom,ylabel="patients")
-    elif chiffre in["reanimation"] and not  evo:
+             intensive care because of Covid-19  in \
+                 "+nom,ylabel="patients")
+
+    elif chiffre in ["reanimation"] and not  evo:
+    
         ax.set(title="Number of patients in intensive\
          care because of Covid-19 in"+nom,ylabel="patients")
+
     elif chiffre in ["cas_confirmes_ehpad"] and evo:
+     
         ax.set(title="Daily cases of Covid-19 in\
          EHPADs"+nom,ylabel="cases")
+
     elif chiffre in ["cas_confirmes_ehpad"] and  not evo:
+
         ax.set(title="Prevalence of Covid-19 in\
              EHPADs"+nom,ylabel="cases")
+
     elif chiffre in ["gueris"] and  not evo:
+
         ax.set(title="Number of people cured from \
             Covid-19 "+nom,ylabel="people")
+
     elif chiffre in ["gueris"] and   evo:
+
         ax.set(title="Daily number of people \
             cured from Covid-19 "+nom,ylabel="people")
 
