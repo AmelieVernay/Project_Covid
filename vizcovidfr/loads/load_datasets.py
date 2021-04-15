c5bdd8eabeb47db20c5314f28f5387e5073c6687
lllinares <laurentllinares@laposte.net>
2021-04-15 22:04:11 +0200

    adding files

--- vizcovidfr/loads/load_datasets.py
+++ vizcovidfr/loads/load_datasets.py
@@ -1,22 +1,29 @@
 from download import download
 import pandas as pd
 import os
+from datetime import date
 
-url = "https://www.data.gouv.fr/en/datasets/r/0b66ca39-1623-4d9c-83ad-5434b7f9e2a4"
+urlcc = "https://www.data.gouv.fr/en/datasets/r/0b66ca39-1623-4d9c-83ad-5434b7f9e2a4"
 path_target = os.path.join(
                 os.path.dirname(
                     os.path.realpath(__file__)),
                 "..", "data", "chiffres-cles.csv")
 
 class Load_chiffres_cles:
-    def __init__(self, url=url, target_name=path_target):
+    def __init__(self, url=urlcc, target_name=path_target):
         download(url, target_name, replace=True)
         # above, set replace to True to always get the updated version
 
     @staticmethod
     def save_as_df():
-        df = pd.read_csv(path_target)
-        return df
+
+        """
+        Nicely formatted dataframe with time indexation.
+        """
+        df_covid = pd.read_csv(path_target)
+
+
+        return df_covid #not have wrong data
 
 url2 = "https://www.data.gouv.fr/fr/datasets/donnees-relatives-aux-stocks-des-doses-de-vaccins-contre-la-covid-19/#_"
 path_target2 = os.path.join(
@@ -32,4 +39,39 @@ class Load_Vaccine_storage:
     def save_as_df():
         df = pd.read_csv(path_target2)
         return df
+
+url_cfr="https://www.data.gouv.fr/fr/datasets/r/d3a98a30-893f-47f7-96c5-2f4bcaaa0d71"
+path_target3 = os.path.join(
+                os.path.dirname(
+                    os.path.realpath(__file__)),
+                "..", "data", "./chiffres-fr.csv")
+
+class Load_chiffres_fr:
+    def __init__(self, url=url_cfr, target_name=path_target3):
+        download(url, target_name, replace=True)
+
+    @staticmethod
+    def save_as_df():
+
+        df_covid = pd.read_csv(path_target3)
+
+        return df_covid
+
+urlposreg="https://www.data.gouv.fr/fr/datasets/r/001aca18-df6a-45c8-89e6-f82d689e6c01"
+path_target4 = os.path.join(
+                os.path.dirname(
+                    os.path.realpath(__file__)),
+                "..", "data", "./posquotreg.csv")
+
+class Load_posquotreg:
+    def __init__(self, url=urlposreg, target_name=path_target):
+        download(url, target_name, replace=True)
+
+    @staticmethod
+    def save_as_df():
+        df = pd.read_csv(path_target2,sep=";")
+        return df
+
+
+
 
