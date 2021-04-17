ade5ae525000c8923505e989f2da2eaced86c975
Quentin Foux <quen.foux@laposte.net>
2021-04-17 17:24:54 +0200

    docstring and vactypedoses function changes

--- vizcovidfr/line_charts/line_charts.py
+++ vizcovidfr/line_charts/line_charts.py
@@ -5,11 +5,10 @@ from download import download
 pd.options.display.max_rows = 25
 import datetime
 import plotly.express as px
-import numpy as np
 
 #from vizcovidfr.loads import load_datasets
 
-#df_vaccine_storage = load_datasets.Load_Vaccine_storage().save_as_df()
+
 
 ##Vaccination part
 #line chart with number of vaccinated people in the whole France
@@ -18,58 +17,66 @@ import numpy as np
 
 url = "https://www.data.gouv.fr/fr/datasets/donnees-relatives-aux-stocks-des-doses-de-vaccins-contre-la-covid-19/#_"
 path_target = "C:/Users/quenf/vizcovidfr/vizcovidfr/data/stocks-es-national.csv"
-download(url, path_target, replace = True)
+#download(url, path_target, replace = True)
 
 def vactypedoses(vaccine_type):
     '''
     Make an animated line chart of France vaccine data.
 
     :param vaccine_type: the vaccine type we want to display.
-        Should be either Pfizer, or Moderna, or AstraZeneca.
+        Should be either 'Pfizer', 'Moderna', 'AstraZeneca' or 'All vaccines'.
+        You can hover your mouse over the curves to get thorough information. 
     :type vaccine_type: string
     :return: an animated line chart representing the actual 
-    dose number or the actual cdu (common dispensing units) 
+    dose number and the actual cdu (common dispensing units) 
     number, of the chosen vaccine type (in storage).
     '''
     df_Vac_type = pd.read_csv(path_target, sep = ",")
-    df_Vac_type = df_Vac_type.groupby(['type_de_vaccin'])
-    pfizer = df_Vac_type.get_group('Pfizer').reset_index(drop = True)
-    mdn = df_Vac_type.get_group('Moderna').reset_index(drop = True)
-    astra = df_Vac_type.get_group('AstraZeneca').reset_index(drop = True)
+    df_Vac_type2 = df_Vac_type.groupby(['type_de_vaccin'])
+    pfizer = df_Vac_type2.get_group('Pfizer').reset_index(drop = True)
+    mdn = df_Vac_type2.get_group('Moderna').reset_index(drop = True)
+    astra = df_Vac_type2.get_group('AstraZeneca').reset_index(drop = True)
     
     # choose dataframe according to vaccine_type argument 
     if (vaccine_type == 'Pfizer'):
         df = pfizer.copy()
         vac_type = 'Pfizer'
+        fig = px.line(df, x = 'date', y = ['nb_doses', 'nb_ucd'], title = 'Pfizer storage in France')
     elif (vaccine_type == 'Moderna'):
         df = mdn.copy()
         vac_type = 'Moderna'
+        fig = px.line(df, x = 'date', y = ['nb_doses', 'nb_ucd'], title = 'Moderna storage in France')
     elif (vaccine_type == 'AstraZeneca'):
         df = astra.copy()
         vac_type == 'AstraZeneca'
+        fig = px.line(df, x = 'date', y = ['nb_doses', 'nb_ucd'], title = 'AstraZeneca storage in France')
+    elif (vaccine_type == 'All vaccines'):
+        df = df_Vac_type.copy()
+        vac_type = 'ALL'
+        fig = px.line(df, x = 'date', y = ['nb_doses', 'nb_ucd'], color = 'type_de_vaccin', title = 'Vaccine storage in France')
     # displaying line chart according to vaccine_type argument
-    fig = px.line(df, x = 'date', y = ['nb_doses', 'nb_ucd'], title = 'Vaccine storage per type')
     fig.show()
+
 #Rather choosing the variables ('nb_doses' and 'nb_ucd') 
 #as arguments or in the animation?
 
 #how to display the year of each date?
-#how can I fix the title according to the chosen vaccine_type?
+
 
 #Test
-#vactypedoses(vaccine_type = 'Moderna')
+vactypedoses(vaccine_type = 'Moderna')
 
 #line chart with total number of vaccine doses
 
 
 def vacdoses(number):
     '''
-    Make an animated line chart of France vaccine data.
+    Make an interactive line chart of France vaccine data.
 
     :param number: the type of dose units we want to display.
         Should be either doses or cdu.
     :type number: string
-    :return: an animated line chart representing the actual
+    :return: an interactive line chart representing the actual
     amount in storage of vaccine doses, according to the chosen 
     unit.
     '''
@@ -90,7 +97,7 @@ def vacdoses(number):
     fig.show()
 
 #Test
-vacdoses(number = 'cdu')
+#vacdoses(number = 'cdu')
 
 #change name of the function and of the argument
 #changedocstring
