c151df7220a888d3da9e5981704ada94fdae4c33
alexxnicolas <alexandrenicolas3@gmail.com>
2021-04-20 19:47:55 +0200

    rename histogram_age by histograms

--- vizcovidfr/histograms/histograms.py
+++ vizcovidfr/histograms/histograms.py
@@ -1,46 +1,94 @@
-#%%
-import csv
-from download import download
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from ipywidgets import interact
+import plotly.express as px
+import plotly.graph_objects as go
+from vizcovidfr.loads import load_datasets
+from vizcovidfr.preprocesses import preprocess_classe_age as pca
 
-#Importation du CSV
-url2 = 'https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv'
-url = "https://www.data.gouv.fr/en/datasets/r/0b66ca39-1623-4d9c-83ad-5434b7f9e2a4"
-path_target = "./chiffres-cles.csv"
-download(url, path_target, replace = True)
+T = load_datasets.Load_classe_age().save_as_df()
 
-T = pd.read_csv("chiffres-cles.csv", sep=',')
-print(T)
-T.columns
+def hist_age(num_var, num_reg):
+    """
+    Display the bar plot of the given variable in the given region by age group today. Each variable and region have a special code that you can see in function parameters for details.
 
-# %%
-#Suppression des 4 dernières colonnes
-data = T.drop(columns=['source_nom', 'source_url', 'source_archive', 'source_type'])
-data
+    Parameters
+    ----------
 
+    :param num_var: code of the variable you want to display. Codes are in the following dictionnary :
 
-# %%
-data.columns = ['Date', 'Granularite', 'Maille code', 'Maille nom', 'Cas confirmés', 'Cas EHPAD', 'Cas confirmés EHPAD', 'Cas possibles EHPAD', 'Deces', 'Deces EHPAD','reanimation', 'hospitalises',
-       'Nouvelles hospitalisations', 'Nouvelles reanimations', 'Gueris', 'Depistes']
-data
-# %%
-data_reg = data[data['Granularite'] == 'region']
-data_reg
-# %%
-covid_today = data_reg.groupby(by='Maille nom').sum()
-covid_today
+    1 : Hospitalization
+    2 : Reanimation
+    3 : Conventional hospitalization
+    4 : SSR and USLD
+    5 : Others
+    6 : Come back home
+    7 : Deaths
 
-# %%
-#Cas confirmés par région
-def case():
-       plt.bar(covid_today.index, covid_today['Cas confirmés'])
-# %%
-#Cas confirmés par région
-def death():
-       plt.bar(covid_today.index, covid_today['Deces'])
-death()
-# %%
+        - Hospitalization :
+            number of hospitalized patients.
+
+        - Reanimation :
+            number of people currently in intensive care or intensive care.
+
+        - Conventional hospitalization :
+            number of people currently in conventional hospitalization.
+
+        - SSR and USLD :
+            number of people currently in Aftercare and Rehabilitation (SSR in french) or Long-Term Care Units (USLD in french).
+    
+        - Others :
+            number of people currently hospitalized in another type of service.
+    
+        - Come back home :
+            cumulative number of people who returned home.
+    
+        - Deaths :
+            cumulative number of deceased persons.
+
+    :type num_var: int (from 1 to 7)
+
+    :param num_reg: code of the region you want to display. Codes are in the following dictionnary (official INSAA code) :
+
+    1 : Guadeloupe
+    2 : Martinique
+    3 : Guyane
+    4 : La Reunion
+    6 : Mayotte
+    11 : Île-de-France
+    24 : Centre-Val de Loire
+    27 : Bourgogne-Franche-Comte
+    28 : Normmandie
+    32 : Hauts-de-France
+    44 : Grand Est
+    52 : Pays de la Loire
+    53 : Bretagne
+    75 : Nouvelle-Aquitaine
+    76 : Occitanie
+    84 : Auvergne-Rhône-Alpes
+    93 : Provence-Alpes Côte d'Azur
+    94 : Corse
+
+    :type num_reg: int
+
+    Returns
+    ----------
+
+    :return: Bar plot of one of the Covid variable in a specific region of France grouped by age.
+    :rtype: plotly.graph_objects.bar.
+
+    """
+    T2 = pca.drop0(T)
+    data_reg = pca.reg(num_reg, T2)
+    dico_col = pca.dico_column(data_reg)
+    data_reg_age = data_reg.groupby(by='cl_age90').sum()
+    data_reg_age['cl_age90'] = data_reg_age.index
+    data_reg_age['cl_age90'] = ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '+90']
+    dico_var = pca.dico_var()
+    dico_reg = pca.dico_reg()
+    fig = px.bar(data_reg_age, x = 'cl_age90', y = dico_col[num_var], 
+                hover_data = [dico_col[num_var]],
+                color = dico_col[num_var],
+                labels = {dico_col[num_var]:dico_var[dico_col[num_var]], 'cl_age90':'Age'},
+                height = 400,
+                title="Bar plot of" + " " + dico_var[dico_col[num_var]] + " in " + dico_reg[num_reg] + " by age group today")
+    fig.show()
