9545ce78299c2fb56b27832b6af02a44ed9a4fe9
alexxnicolas <alexandrenicolas3@gmail.com>
2021-04-23 19:04:07 +0200

    add dataframe in function

--- vizcovidfr/prediction/prediction.py
+++ vizcovidfr/prediction/prediction.py
@@ -19,8 +19,6 @@ from vizcovidfr.preprocesses import preprocess_classe_age as pca
 from vizcovidfr.regression import regression as rg
 
 
-T = load_datasets.Load_classe_age().save_as_df()
-
 
 def predict_curve(num_var, num_reg, date = 'date', save = False):
     """
@@ -135,47 +133,50 @@ def predict_curve(num_var, num_reg, date = 'date', save = False):
     >>> predict_curve(1,93,'2021-04-30') 
 
     """
+    # Testing execution time
     start = time.time()
-    #Choose region
+    # Loading dataframe
+    T = load_datasets.Load_classe_age().save_as_df()
+    # Choose region
     R = pca.reg(num_reg, T)
-    #Convert to datetime format
+    # Convert to datetime format
     R = pca.date_time(R)
-    #Columns dictionary
+    # Columns dictionary
     dico_col = pca.dico_column(R)
-    #Grouping by day
+    # Grouping by day
     covid_day = pca.covid_day_fct(R)
-    #Choose variable to explain
+    # Choose variable to explain
     x = np.arange(0, covid_day.shape[0])
     y = covid_day[dico_col[1]]
     x = x[:, np.newaxis]
     y = y[:, np.newaxis]
-    #Dictionaries
+    # Dictionaries
     dico_days = pca.dico_day(covid_day)
     dico_file = pca.dico_file()
     dico_var = pca.dico_var()
     dico_reg = pca.dico_reg()
     covid_day = covid_day.reset_index(drop=True)
-    #Mean squared error and prediction of y
+    # Mean squared error and prediction of y
     rmselist, x_p_list, y_poly_pred_P_list = pca.rmse_list(x, y)
     deg = list(rmselist).index(rmselist.min())
-    #Coefficients of the regression polynom
+    # Coefficients of the regression polynom
     coefs = np.polyfit(np.arange(0, covid_day.shape[0]), y_poly_pred_P_list[deg], deg+1)
-    #Date format
+    # Date format
     delta = datetime.timedelta(days=1)
     date2 = pd.to_datetime(date)
-    #Listing dico_days keys
+    # Listing dico_days keys
     keys = list(dico_days.keys())
-    #Dictionary dico_days with date until 'date'
+    # Dictionary dico_days with date until 'date'
     period = date2 - dico_days[keys[-1]]
     num_date = keys[-1] + period.days
     dico_days2 = dico_days.copy()
     for i in np.arange(keys[-1]+1,num_date+1):
         dico_days2[i] = dico_days2[i-1] + delta
-    #Scatter plot with the predicted regression line
+    # Scatter plot with the predicted regression line
     fig = plt.scatter(dico_days.values(), y)
     plt.plot(dico_days2.values(), np.polyval(coefs, np.arange(num_date+1)), color="black")
     plt.title("Prediction curve of" + " " + dico_var[dico_col[num_var]] + " in " + dico_reg[num_reg] + "\n until " + date)
-    #Saving pdf file
+    # Saving pdf file
     if save == True:
         plt.savefig(f"predict_" + dico_file[num_var] + "_" + dico_reg[num_reg] + "_" + date + ".pdf", dpi=1200)
     end = time.time()
@@ -294,48 +295,51 @@ def predict_value(num_var, num_reg, date = 'date'):
     >>> predict_value(1,93,'2021-04-30') 
 
     """
+    # Testing execution time
     start = time.time()
-    #Choose region
+    # Loading dataframe
+    T = load_datasets.Load_classe_age().save_as_df()
+    # Choose region
     R = pca.reg(num_reg, T)
-    #Convert to datetime format
+    # Convert to datetime format
     R = pca.date_time(R)
-    #Columns dictionary
+    # Columns dictionary
     dico_col = pca.dico_column(R)
-    #Grouping by day
+    # Grouping by day
     covid_day = pca.covid_day_fct(R)
-    #Choose variable to explain
+    # Choose variable to explain
     x = np.arange(0, covid_day.shape[0])
     y = covid_day[dico_col[1]]
     x = x[:, np.newaxis]
     y = y[:, np.newaxis]
-    #Dictionaries
+    # Dictionaries
     dico_days = pca.dico_day(covid_day)
     dico_var = pca.dico_var()
     dico_reg = pca.dico_reg()
     covid_day = covid_day.reset_index(drop=True)
-    #Mean squared error and prediction of y
+    # Mean squared error and prediction of y
     rmselist, x_p_list, y_poly_pred_P_list = pca.rmse_list(x, y)
     deg = list(rmselist).index(rmselist.min())
-    #Coefficients of the regression polynom
+    # Coefficients of the regression polynom
     coefs = np.polyfit(np.arange(0, covid_day.shape[0]), y_poly_pred_P_list[deg], deg+1)
-    #Date format
+    # Date format
     delta = datetime.timedelta(days=1)
     date2 = pd.to_datetime(date)
-    #Listing dico_days keys
+    # Listing dico_days keys
     keys = list(dico_days.keys())
-    #Dictionary dico_days with date until 'date'
+    # Dictionary dico_days with date until 'date'
     period = date2 - dico_days[keys[-1]]
     num_date = keys[-1] + period.days
     dico_days2 = dico_days.copy()
     for i in np.arange(keys[-1]+1,num_date+1):
         dico_days2[i] = dico_days2[i-1] + delta
-    #Predicted value
+    # Predicted value
     pred = np.polyval(coefs, num_date+1)[0]
     if num_var == 6 or num_var == 7:
         res = 'The cumulative number of ' + dico_var[dico_col[num_var]] + " in " + dico_reg[num_reg] + " on " + date + f' will be {round(pred)}.' 
     else:
         res = 'The number of ' + dico_var[dico_col[num_var]] + " in " + dico_reg[num_reg] + " on " + date + f' will be {round(pred)}.'
-        end = time.time()
+    end = time.time()
     print("Time to execute: {0:.5f} s.".format(end - start))
     return res
 
