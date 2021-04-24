63e0d4aaf6666c68886f62d05efd1d00eb717d6b
AmelieVernay <amelie.vernay@etu.umontpellier.fr>
2021-04-24 22:11:15 +0200

    change theme :'(

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,7 @@
 #
 # import os
 # import sys
-# import faculty-sphinx-theme
+import sphinx_rtd_theme
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -32,7 +32,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	"faculty_sphinx_theme",
+	"sphinx_rtd_theme",
 	"sphinx.ext.autodoc",
 	"sphinx_gallery.gen_gallery",
 ]
@@ -51,7 +51,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'faculty-sphinx-theme'
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
