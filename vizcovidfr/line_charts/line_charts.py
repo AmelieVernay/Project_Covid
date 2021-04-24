33c81463db485e5c42474a6b2eeedda77f66e322
Quentin Foux <quen.foux@laposte.net>
2021-04-24 13:46:28 +0200

    cdu unit works again

--- vizcovidfr/line_charts/line_charts.py
+++ vizcovidfr/line_charts/line_charts.py
@@ -226,7 +226,7 @@ def vacdoses(unit='doses', font_size=16,
     doses['nb_doses'] = df['nb_doses']
     doses.rename(columns={'nb_doses': 'Number of doses with basic unit', 'date': 'Date'}, inplace=True)
     ucd['nb_ucd'] = df['nb_ucd']
-    ucd.rename(columns={'nb_doses': 'Number of doses with ucd unit', 'date': 'Date'}, inplace=True)
+    ucd.rename(columns={'nb_ucd': 'Number of doses with ucd unit', 'date': 'Date'}, inplace=True)
     if (unit == 'doses'):
         df = doses.copy()
         nbr = 'Number of doses with basic unit'
