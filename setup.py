9a418da9adeea186919438500c3d40f4ba93cf42
AmelieVernay <amelie.vernay@etu.umontpellier.fr>
2021-04-24 20:33:09 +0200

    update setup again

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description='A python package to visualize spatial evolution of Covid19 cases in France',
   url='https://github.com/AmelieVernay/vizcovidfr',
   author='Foux Quentin ; Llinares Laurent ; Nicolas Alexandre ; Vernay Amelie',
-  author_email='quentin.foux@etu.umontpellier.fr ; laurent.llinares@etu.umontpellier.fr ; alexandre.nicolas@etu.umontpellier.fr ; amelie.vernay@etu.umontpellier.fr',
+  author_email='quentin.foux@etu.umontpellier.fr, laurent.llinares@etu.umontpellier.fr, alexandre.nicolas@etu.umontpellier.fr, amelie.vernay@etu.umontpellier.fr',
   license='MIT',
   zip_safe=False,
   packages=find_packages(include=['vizcovidfr',
